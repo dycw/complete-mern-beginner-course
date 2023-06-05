41c7847d8577a0cce155b8a91fee096855fb71f7
Derek Wan <d.wan@icloud.com>
2023-06-05 10:46:41 +0800

    Finish lesson 4 (MongoDB model, Express error handling) (#6)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,30 @@
+import NoteModel from "./models/note";
 import "dotenv/config";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 
 const app = express();
 
-app.get("/", (_req, res) => res.send("Hello, world!"));
+app.get("/", async (_req, res, next) => {
+  try {
+    // throw Error("Bazinga!");
+    const notes = await NoteModel.find().exec();
+    res.status(200).json(notes);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((_req, _res, next) => {
+  next(Error("Endpoint not found"));
+});
+
+app.use((error: unknown, _req: Request, res: Response, _next: NextFunction) => {
+  console.error(error);
+  let errorMessage = "An unknown error occurred";
+  if (error instanceof Error) {
+    errorMessage = error.message;
+  }
+  res.status(500).json({ error: errorMessage });
+});
 
 export default app;
