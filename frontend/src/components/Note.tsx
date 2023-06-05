038eb9e19838b12896411d086f5f300120c56e33
Derek Wan <d.wan@icloud.com>
2023-06-05 15:21:35 +0800

    Finish lesson 11 (finish notes styling)

--- frontend/src/components/Note.tsx
+++ frontend/src/components/Note.tsx
@@ -1,18 +1,31 @@
-import { Card } from "react-bootstrap";
+import { Note as NoteModel } from "../models/note";
 import styles from "../styles/Note.module.css";
-import { Note } from "../models/note";
+import formatDate from "../utils/formatDate";
+import { Card } from "react-bootstrap";
 
 type Props = {
-	note: Note;
+  note: NoteModel;
+  className?: string;
 };
 
-export default function Note({ note: { title, text } }: Props) {
-	return (
-		<Card className={styles.noteCard}>
-			<Card.Body>
-				<Card.Title>{title}</Card.Title>
-				<Card.Text className={styles.cardText}>{text}</Card.Text>
-			</Card.Body>
-		</Card>
-	);
+export default function Note({
+  note: { title, text, createdAt, updatedAt },
+  className,
+}: Props) {
+  let createdUpdatedText: string;
+  if (updatedAt > createdAt) {
+    createdUpdatedText = `Updated: ${formatDate(updatedAt)}`;
+  } else {
+    createdUpdatedText = `Created: ${formatDate(createdAt)}`;
+  }
+
+  return (
+    <Card className={`${styles.noteCard} ${className}`}>
+      <Card.Body className={styles.cardBody}>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text className={styles.cardText}>{text}</Card.Text>
+      </Card.Body>
+      <Card.Footer className="text-muted">{createdUpdatedText}</Card.Footer>
+    </Card>
+  );
 }
