01a22dfeb4360f89537e4cc5bf4818dd17804516
Derek Wan <d.wan@icloud.com>
2023-06-05 12:34:38 +0800

    Finish lesson 9 (fetching notes)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,26 @@
-import { useState } from "react";
-import { Button } from "react-bootstrap";
+import { Note } from "./models/note";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [clickCount, setClickCount] = useState(0);
+  const [notes, setNotes] = useState<Note[]>([]);
 
-  return (
-    <>
-      <h1>React Template</h1>
-      <Button onClick={() => setClickCount((c) => c + 1)}>
-        Clicked {clickCount} times
-      </Button>
-    </>
-  );
+  useEffect(() => {
+    async function loadNotes() {
+      try {
+        const response = await fetch("http://localhost:5001/api/notes", {
+          method: "GET",
+        });
+        const notes = await response.json();
+        setNotes(notes);
+      } catch (error) {
+        console.error(error);
+        alert(error);
+      }
+    }
+    loadNotes();
+  }, []);
+
+  return <div className="App">{JSON.stringify(notes)}</div>;
 }
 
 export default App;
