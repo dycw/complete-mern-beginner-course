447a32738ff7f05ea79ffe06dde8c16e1f6867d5
Derek Wan <d.wan@icloud.com>
2023-06-05 16:43:07 +0800

    Finish lesson 13 (updating & deleting notes) (#16)

--- frontend/src/components/Note.tsx
+++ frontend/src/components/Note.tsx
@@ -1,17 +1,24 @@
 import { Note as NoteModel } from "../models/note";
 import styles from "../styles/Note.module.css";
+import styleUtils from "../styles/utils.module.css";
 import formatDate from "../utils/formatDate";
 import { Card } from "react-bootstrap";
+import { MdDelete } from "react-icons/md";
 
 type Props = {
   note: NoteModel;
+  onNoteClicked: (note: NoteModel) => void;
+  onDeleteNoteClicked: (note: NoteModel) => void;
   className?: string;
 };
 
 export default function Note({
-  note: { title, text, createdAt, updatedAt },
+  note,
+  onNoteClicked,
+  onDeleteNoteClicked,
   className,
 }: Props) {
+  const { title, text, createdAt, updatedAt } = note;
   let createdUpdatedText: string;
   if (updatedAt > createdAt) {
     createdUpdatedText = `Updated: ${formatDate(updatedAt)}`;
@@ -20,9 +27,21 @@ export default function Note({
   }
 
   return (
-    <Card className={`${styles.noteCard} ${className}`}>
+    <Card
+      className={`${styles.noteCard} ${className}`}
+      onClick={() => onNoteClicked(note)}
+    >
       <Card.Body className={styles.cardBody}>
-        <Card.Title>{title}</Card.Title>
+        <Card.Title className={styleUtils.flexCenter}>
+          {title}
+          <MdDelete
+            className="text-muted ms-auto"
+            onClick={(e) => {
+              onDeleteNoteClicked(note);
+              e.stopPropagation();
+            }}
+          />
+        </Card.Title>
         <Card.Text className={styles.cardText}>{text}</Card.Text>
       </Card.Body>
       <Card.Footer className="text-muted">{createdUpdatedText}</Card.Footer>
--- frontend/src/network/notes_api.ts
+++ frontend/src/network/notes_api.ts
@@ -30,3 +30,24 @@ export async function createNote(note: NoteInput): Promise<Note> {
   });
   return response.json();
 }
+
+export async function updateNote(
+  noteId: string,
+  note: NoteInput
+): Promise<Note> {
+  const response = await fetchData(
+    `http://localhost:5001/api/notes/${noteId}`,
+    {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(note),
+    }
+  );
+  return response.json();
+}
+
+export async function deleteNote(noteId: string) {
+  await fetchData(`http://localhost:5001/api/notes/${noteId}`, {
+    method: "DELETE",
+  });
+}
